ec3349992f503abd42126e67cd74ddce774b7032
seongbin-hong <qls056@gamil.com>
2023-04-07 22:46:40 +0900

    ios firebase libs 연동

--- ios/villife/AppDelegate.mm
+++ ios/villife/AppDelegate.mm
@@ -1,5 +1,5 @@
 #import "AppDelegate.h"
-
+#import <Firebase.h>
 #import <React/RCTBundleURLProvider.h>
 
 // Added for Naver login.
@@ -10,6 +10,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [FIRApp configure];
   self.moduleName = @"villife";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
--- ios/villife/FirebasePushNotifier.swift
+++
@@ -1,8 +0,0 @@
-//
-//  FirebasePushNotifier.swift
-//  villife
-//
-//  Created by 홍성빈 on 2023/04/02.
-//
-
-import Foundation
