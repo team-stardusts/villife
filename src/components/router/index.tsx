ac5a488ca4ab15622a0ff154ce70c4925b6e7a67
seongbin-hong <qls056@gamil.com>
2023-04-08 15:35:22 +0900

    iOS firebase token 획득 성공

--- src/components/router/index.tsx
+++ src/components/router/index.tsx
@@ -1,76 +1,66 @@
-import {useEffect, useState} from 'react';
-import {enableScreens} from 'react-native-screens';
-import {useRecoilState} from 'recoil';
-import {isLoggedInState} from '../../hooks/states/atoms/login';
-import useLoginSession from '../../hooks/session';
-import {
-  createNativeStackNavigator,
-  NativeStackScreenProps,
-} from '@react-navigation/native-stack';
-import {StackParamList} from './types';
-import LoginScreen from '../screens/auth/login';
-import CreateAccountScreen from '../screens/auth/create_account';
-import SetBuildingScreen from '../screens/auth/set_building';
-import TermsOfServiceScreen from '../screens/auth/terms_of_service/index.';
-import SearchAddressScreen from '../screens/reusable/search_address';
-import HomeScreen from '../screens/main/home';
-import SplashScreen from '../screens/splash/splash_screen';
-import {NavigationContainer, useNavigation} from '@react-navigation/native';
-import TestScreen from '../screens/test';
+import { useEffect, useState } from "react";
+import { enableScreens } from "react-native-screens";
+import { useRecoilState } from "recoil";
+import { isLoggedInState } from "../../hooks/states/atoms/login";
+import useLoginSession from "../../hooks/session";
+import { createNativeStackNavigator, NativeStackScreenProps } from "@react-navigation/native-stack";
+import { StackParamList } from "./types";
+import LoginScreen from "../screens/auth/login";
+import CreateAccountScreen from "../screens/auth/create_account";
+import SetBuildingScreen from "../screens/auth/set_building";
+import TermsOfServiceScreen from "../screens/auth/terms_of_service/index.";
+import SearchAddressScreen from "../screens/reusable/search_address";
+import HomeScreen from "../screens/main/home";
+import SplashScreen from "../screens/splash/splash_screen";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
+import TestScreen from "../screens/test";
 
 enableScreens(true);
 
 const Stack = createNativeStackNavigator<StackParamList>();
 
-type test = NativeStackScreenProps<StackParamList>;
+type RouterParams = NativeStackScreenProps<StackParamList>;
 
 export default function ScreenRouter() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [isLoggedIn, setIsLoggedIn] = useRecoilState<boolean | null>(
-    isLoggedInState,
-  );
-  const navigation = useNavigation<test['navigation']>();
+    const [isLoading, setIsLoading] = useState(true);
+    const [isLoggedIn, setIsLoggedIn] = useRecoilState<boolean | null>(isLoggedInState);
+    const navigation = useNavigation<RouterParams["navigation"]>();
 
-  useLoginSession();
-  // [TO-DO] Code 정리
-  useEffect(() => {
-    if (isLoggedIn === null) {
-      setIsLoading(true);
-      //navigation.navigate('test', {});
-      //navigation.navigate("splash", {});
-    } else if (isLoggedIn === true) {
-      setIsLoading(false);
-      navigation.navigate('home', {});
-    } else {
-      setIsLoading(false);
-    }
-  }, [isLoggedIn]);
+    useLoginSession();
+    // [TO-DO] Code 정리
+    useEffect(() => {
+        if (isLoggedIn === null) {
+            setIsLoading(true);
+            //navigation.navigate('test', {});
+            //navigation.navigate("splash", {});
+        } else if (isLoggedIn === true) {
+            setIsLoading(false);
+            navigation.navigate("home", {});
+        } else {
+            setIsLoading(false);
+        }
+    }, [isLoggedIn]);
 
-  return (
-    <Stack.Navigator
-      screenOptions={{headerShown: false}}
-      initialRouteName={'login'}>
-      <Stack.Group>
-        <Stack.Screen name={'login'} component={LoginScreen} />
-        <Stack.Screen
-          name={'terms_of_service'}
-          component={TermsOfServiceScreen}
-        />
-        <Stack.Screen name={'create_account'} component={CreateAccountScreen} />
-        <Stack.Screen name={'set_building'} component={SetBuildingScreen} />
-      </Stack.Group>
-      <Stack.Group screenOptions={{headerShown: true}}>
-        <Stack.Screen name={'home'} component={HomeScreen} />
-      </Stack.Group>
-      <Stack.Group>
-        <Stack.Screen name={'splash'} component={SplashScreen} />
-      </Stack.Group>
-      <Stack.Group screenOptions={{presentation: 'modal'}}>
-        <Stack.Screen name={'search_address'} component={SearchAddressScreen} />
-      </Stack.Group>
-      <Stack.Group>
-        <Stack.Screen name={'test'} component={TestScreen} />
-      </Stack.Group>
-    </Stack.Navigator>
-  );
+    return (
+        <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName={"login"}>
+            <Stack.Group>
+                <Stack.Screen name={"login"} component={LoginScreen} />
+                <Stack.Screen name={"terms_of_service"} component={TermsOfServiceScreen} />
+                <Stack.Screen name={"create_account"} component={CreateAccountScreen} />
+                <Stack.Screen name={"set_building"} component={SetBuildingScreen} />
+            </Stack.Group>
+            <Stack.Group screenOptions={{ headerShown: true }}>
+                <Stack.Screen name={"home"} component={HomeScreen} />
+            </Stack.Group>
+            <Stack.Group>
+                <Stack.Screen name={"splash"} component={SplashScreen} />
+            </Stack.Group>
+            <Stack.Group screenOptions={{ presentation: "modal" }}>
+                <Stack.Screen name={"search_address"} component={SearchAddressScreen} />
+            </Stack.Group>
+            <Stack.Group>
+                <Stack.Screen name={"test"} component={TestScreen} />
+            </Stack.Group>
+        </Stack.Navigator>
+    );
 }
--- src/components/screens/test/index.tsx
+++ src/components/screens/test/index.tsx
@@ -1,40 +1,69 @@
-import {
-  NativeModules,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-} from 'react-native';
-import {useGetFirebaseToken} from '../../../hooks/firebase/hooks';
-import AndroidFirebaseModule from '../../../hooks/firebase/android_module';
+import { NativeModules, PermissionsAndroid, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { useGetFirebaseToken } from "../../../hooks/firebase/hooks";
+import AndroidFirebaseModule from "../../../hooks/firebase/android_module";
+import { firebase } from "@react-native-firebase/messaging";
+import useSystemInfo from "../../../hooks/systeminfo/hooks";
 
 export default function TestScreen() {
-  const firebaseToken = useGetFirebaseToken();
+    const firebaseToken = useGetFirebaseToken();
+    const sysinfo = useSystemInfo();
 
-  return (
-    <View>
-      <View>
-        <TouchableOpacity
-          onPress={() => {
-            console.log(firebaseToken);
-          }}
-          style={localStyle.testButton}>
-          <Text style={localStyle.buttonText}>버튼</Text>
-        </TouchableOpacity>
-      </View>
-    </View>
-  );
+    const { messaging } = firebase;
+
+    const getFirebaseToken = async () => {
+        let authStatus: any = null;
+        let isEnabled: boolean = true;
+
+        if (sysinfo.platform.OS === "ios") {
+            const authStatus = await messaging().requestPermission();
+            isEnabled =
+                authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+                authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+        } else if (sysinfo.platform.OS === "android") {
+            authStatus = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.READ_CONTACTS, {
+                title: "Notification Permission",
+                message: "ExampleApp needs access to your notifications.",
+                buttonNeutral: "Ask Me Later",
+                buttonNegative: "Cancel",
+                buttonPositive: "OK",
+            });
+        } else {
+            throw new Error("The user is using an unexpected OS.");
+        }
+
+        if (isEnabled && sysinfo.platform.OS === "android") {
+            const token = await messaging().getAPNSToken();
+            console.log("auth", authStatus);
+            console.log("token", token);
+        }
+    };
+
+    getFirebaseToken();
+
+    return (
+        <View>
+            <View>
+                <TouchableOpacity
+                    onPress={() => {
+                        console.log(firebaseToken);
+                    }}
+                    style={localStyle.testButton}>
+                    <Text style={localStyle.buttonText}>버튼</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
 }
 
 const localStyle = StyleSheet.create({
-  testButton: {
-    width: '100%',
-    backgroundColor: 'red',
-    height: 50,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  buttonText: {
-    fontSize: 20,
-  },
+    testButton: {
+        width: "100%",
+        backgroundColor: "red",
+        height: 50,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    buttonText: {
+        fontSize: 20,
+    },
 });
