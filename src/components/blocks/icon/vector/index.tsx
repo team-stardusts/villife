36821103c3fdf0aa85b274b707b8cf53e218d942
malonabi <dnsi37>
2023-04-11 23:00:17 +0900

    공지 홈 구현중

--- src/components/blocks/icon/vector/index.tsx
+++ src/components/blocks/icon/vector/index.tsx
@@ -1,194 +1,158 @@
-import {useState} from 'react';
-import {Pressable, View} from 'react-native';
-import {Svg, Rect, Defs, Pattern, Use, Image, Path} from 'react-native-svg';
-import {
-  PressableIconProps,
-  PressableVectorIconProps,
-  VectorIconProps,
-  VectorIconReturnType,
-} from './types';
+import { useState } from "react";
+import { Pressable, View } from "react-native";
+import { Svg, Rect, Defs, Pattern, Use, Image, Path } from "react-native-svg";
+import { PressableIconProps, PressableVectorIconProps, VectorIconProps, VectorIconReturnType } from "./types";
 
 function RightVectorIcon(props: VectorIconProps): VectorIconReturnType {
-  const {diameter} = props;
+    const { diameter } = props;
 
-  return (
-    <Svg
-      width={diameter}
-      height={diameter}
-      viewBox="0 0 24 24"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg">
-      <Path
-        d="M9 18L15 12L9 6"
-        stroke="#191F26"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-      />
-    </Svg>
-  );
+    return (
+        <Svg width={diameter} height={diameter} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <Path
+                d="M9 18L15 12L9 6"
+                stroke="#191F26"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+            />
+        </Svg>
+    );
 }
 
 function LeftVectorIcon(props: VectorIconProps): VectorIconReturnType {
-  const {diameter} = props;
+    const { diameter } = props;
 
-  return (
-    <Svg
-      width={diameter}
-      height={diameter}
-      viewBox={`0 0 ${diameter} ${diameter}`}
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg">
-      <Path
-        d="M15 18L9 12L15 6"
-        stroke="#191F26"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-      />
-    </Svg>
-  );
+    return (
+        <Svg
+            width={diameter}
+            height={diameter}
+            viewBox={`0 0 ${diameter} ${diameter}`}
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg">
+            <Path
+                d="M15 18L9 12L15 6"
+                stroke="#191F26"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+            />
+        </Svg>
+    );
 }
 
 function UpVectorIcon(props: VectorIconProps): VectorIconReturnType {
-  const {diameter} = props;
+    const { diameter } = props;
 
-  return (
-    <Svg
-      width={diameter}
-      height={diameter}
-      viewBox={`0 0 ${diameter} ${diameter}`}
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg">
-      <Path
-        d="M18 15L12 9L6 15"
-        stroke="#191F26"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-      />
-    </Svg>
-  );
+    return (
+        <Svg width={diameter} height={diameter} viewBox={`0 0 24 24`} fill="none" xmlns="http://www.w3.org/2000/svg">
+            <Path
+                d="M18 15L12 9L6 15"
+                stroke="#191F26"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+            />
+        </Svg>
+    );
 }
 
 function DownVectorIcon(props: VectorIconProps): VectorIconReturnType {
-  const {diameter} = props;
+    const { diameter } = props;
 
-  return (
-    <Svg
-      width={diameter}
-      height={diameter}
-      viewBox={`0 0 ${diameter} ${diameter}`}
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg">
-      <Path
-        d="M6 9L12 15L18 9"
-        stroke="#191F26"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-      />
-    </Svg>
-  );
+    return (
+        <Svg width={diameter} height={diameter} viewBox={`0 0 24 24`} fill="none">
+            <Path
+                d="M6 9L12 15L18 9"
+                stroke="#191F26"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+            />
+        </Svg>
+    );
 }
 
 function PlusVectorIcon(props: VectorIconProps): VectorIconReturnType {
-  const {diameter} = props;
+    const { diameter } = props;
 
-  return (
-    <Svg
-      width={diameter}
-      height={diameter}
-      viewBox={`0 0 ${diameter} ${diameter}`}
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg">
-      <Path
-        d="M12 5V19"
-        stroke="#191F26"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-      />
-      <Path
-        d="M5 12H19"
-        stroke="#191F26"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-      />
-    </Svg>
-  );
+    return (
+        <Svg
+            width={diameter}
+            height={diameter}
+            viewBox={`0 0 ${diameter} ${diameter}`}
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg">
+            <Path d="M12 5V19" stroke="#191F26" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+            <Path d="M5 12H19" stroke="#191F26" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+        </Svg>
+    );
 }
 
 function PressableIcon(props: PressableIconProps): JSX.Element {
-  const {diameter} = props;
-  const radius = diameter / 2;
-  const pressedOpacity: number = 0.2;
-  const unpressedOpacity: number = 0;
-  const [opacity, setOpacity] = useState(unpressedOpacity);
+    const { diameter } = props;
+    const radius = diameter / 2;
+    const pressedOpacity: number = 0.2;
+    const unpressedOpacity: number = 0;
+    const [opacity, setOpacity] = useState(unpressedOpacity);
 
-  return (
-    <Pressable
-      onPress={props.onPress}
-      onPressIn={() => (props.onPress ? setOpacity(pressedOpacity) : undefined)}
-      onPressOut={() =>
-        props.onPress ? setOpacity(unpressedOpacity) : undefined
-      }>
-      {props.children}
-      <View
-        style={{
-          position: 'absolute',
-          top: 0,
-          bottom: 0,
-          left: 0,
-          right: 0,
-          width: diameter,
-          height: diameter,
-          backgroundColor: 'grey',
-          borderRadius: radius,
-          opacity: opacity,
-          zIndex: 1,
-        }}
-      />
-    </Pressable>
-  );
+    return (
+        <Pressable
+            onPress={props.onPress}
+            onPressIn={() => (props.onPress ? setOpacity(pressedOpacity) : undefined)}
+            onPressOut={() => (props.onPress ? setOpacity(unpressedOpacity) : undefined)}>
+            {props.children}
+            <View
+                style={{
+                    position: "absolute",
+                    top: 0,
+                    bottom: 0,
+                    left: 0,
+                    right: 0,
+                    width: diameter,
+                    height: diameter,
+                    backgroundColor: "grey",
+                    borderRadius: radius,
+                    opacity: opacity,
+                    zIndex: 1,
+                }}
+            />
+        </Pressable>
+    );
 }
 
-export default function PressableVectorIcon(
-  props: PressableVectorIconProps,
-): VectorIconReturnType {
-  const {providerName, diameter} = props;
+export default function PressableVectorIcon(props: PressableVectorIconProps): VectorIconReturnType {
+    const { providerName, diameter } = props;
 
-  switch (providerName) {
-    case 'right':
-      return (
-        <PressableIcon diameter={diameter} onPress={props.onPress}>
-          <RightVectorIcon diameter={diameter} />
-        </PressableIcon>
-      );
-    case 'left':
-      return (
-        <PressableIcon diameter={diameter} onPress={props.onPress}>
-          <LeftVectorIcon diameter={diameter} />
-        </PressableIcon>
-      );
-    case 'up':
-      return (
-        <PressableIcon diameter={diameter} onPress={props.onPress}>
-          <UpVectorIcon diameter={diameter} />
-        </PressableIcon>
-      );
-    case 'down':
-      return (
-        <PressableIcon diameter={diameter} onPress={props.onPress}>
-          <DownVectorIcon diameter={diameter} />
-        </PressableIcon>
-      );
-    default:
-      return (
-        <PressableIcon diameter={diameter} onPress={props.onPress}>
-          <PlusVectorIcon diameter={diameter} />
-        </PressableIcon>
-      );
-  }
+    switch (providerName) {
+        case "right":
+            return (
+                <PressableIcon diameter={diameter} onPress={props.onPress}>
+                    <RightVectorIcon diameter={diameter} />
+                </PressableIcon>
+            );
+        case "left":
+            return (
+                <PressableIcon diameter={diameter} onPress={props.onPress}>
+                    <LeftVectorIcon diameter={diameter} />
+                </PressableIcon>
+            );
+        case "up":
+            return (
+                <PressableIcon diameter={diameter} onPress={props.onPress}>
+                    <UpVectorIcon diameter={diameter} />
+                </PressableIcon>
+            );
+        case "down":
+            return (
+                <PressableIcon diameter={diameter} onPress={props.onPress}>
+                    <DownVectorIcon diameter={diameter} />
+                </PressableIcon>
+            );
+        default:
+            return (
+                <PressableIcon diameter={diameter} onPress={props.onPress}>
+                    <PlusVectorIcon diameter={diameter} />
+                </PressableIcon>
+            );
+    }
 }
--- src/components/screens/noti/home/index.tsx
+++ src/components/screens/noti/home/index.tsx
@@ -1,10 +1,12 @@
-import { Pressable, TouchableHighlight, View } from "react-native";
+import { Dimensions, Pressable, StyleSheet, View } from "react-native";
 import NoticeHomeScreenProps from "./type";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { NativeModules, Text } from "react-native";
 import React from "react";
 import Button from "../../../atoms/button";
 import { LayoutAnimation } from "react-native";
+import { Image } from "react-native-svg";
+import PressableVectorIcon from "../../../blocks/icon/vector/index";
 
 function NoticeHomeScreen(props: NoticeHomeScreenProps) {
     return (
@@ -12,16 +14,21 @@ function NoticeHomeScreen(props: NoticeHomeScreenProps) {
             <View style={{ width: "100%", height: "10%", backgroundColor: "red" }}>
                 <Text>Top Nav</Text>
             </View>
-            <NotiItem />
-            <NotiItem />
-            <NotiItem />
-            <NotiItem />
-            <NotiItem />
-            <NotiItem />
-            <NotiItem />
-            <NotiItem />
-            <NotiItem />
-            <NotiItem />
+            <View style={{ height: "80%", width: "100%", alignItems: "center" }}>
+                <NotiItem />
+                <NotiItem />
+                <NotiItem />
+                <NotiItem />
+                <NotiItem />
+                <NotiItem />
+                <NotiItem />
+                <NotiItem />
+                <NotiItem />
+                <NotiItem />
+            </View>
+            <View style={{ width: "100%", height: "10%", backgroundColor: "red" }}>
+                <Text>Bottom Nav</Text>
+            </View>
         </SafeAreaView>
     );
 }
@@ -37,31 +44,127 @@ type NotiItemProps = {
 
 function NotiItem(props: NotiItemProps) {
     const [unfold, setUnfold] = React.useState(false);
+    const size = Dimensions.get("window");
 
     const onPress = () => {
         setUnfold(!unfold);
-        LayoutAnimation.linear();
+        LayoutAnimation.configureNext({
+            duration: 100,
+            update: {
+                type: LayoutAnimation.Types.linear,
+            },
+        });
     };
 
     return (
         <>
-            {unfold ? (
-                <TouchableHighlight
-                    onPressOut={() => {
-                        onPress();
-                    }}
-                    style={{ width: "100%", height: "20%", backgroundColor: "blue" }}>
-                    <View></View>
-                </TouchableHighlight>
-            ) : (
-                <TouchableHighlight
-                    onPressOut={() => {
-                        onPress();
-                    }}
-                    style={{ width: "100%", height: "10%", backgroundColor: "blue" }}>
-                    <View></View>
-                </TouchableHighlight>
-            )}
+            <Pressable
+                onPressOut={() => {
+                    if (!unfold) onPress();
+                }}
+                style={[NotiItemStyle.container, { minHeight: size.height * 0.1 * 0.8 }]}>
+                <View style={NotiItemStyle.innerBox}>
+                    <View
+                        style={[
+                            NotiItemStyle.innerTitleSection,
+                            {
+                                height: size.height * 0.1 * 0.8,
+                                borderBottomWidth: !unfold ? 0 : 2,
+                                borderBottomColor: "#0B75F2",
+                            },
+                        ]}>
+                        <NotiLable priority={0} />
+                        <View style={NotiItemStyle.titleTextBox}>
+                            <Text style={[]}>공지사항제목</Text>
+                            <Text style={[]}>2023-01</Text>
+                        </View>
+                        <View style={NotiItemStyle.absoluteWrapper}>
+                            <PressableVectorIcon
+                                onPress={() => {
+                                    onPress();
+                                }}
+                                providerName={unfold ? "up" : "down"}
+                                diameter={30}
+                            />
+                        </View>
+                    </View>
+                    {unfold && (
+                        <Text>
+                            {
+                                "hello \n hello \n hello \n hello \n hello \n hello \n hello \n hello \n hello \n hello \n hello \n hello \n hello \n hello \n"
+                            }
+                        </Text>
+                    )}
+                </View>
+            </Pressable>
         </>
     );
 }
+
+const NotiItemStyle = StyleSheet.create({
+    container: {
+        width: "90%",
+        borderWidth: 2,
+        borderColor: "#0B75F2",
+        marginVertical: "1.5%",
+        borderRadius: 15,
+        overflow: "hidden",
+    },
+    innerBox: {
+        alignItems: "center",
+        overflow: "visible",
+    },
+    innerTitleSection: {
+        width: "90%",
+        flexDirection: "row",
+        alignItems: "center",
+    },
+    titleTextBox: {
+        marginLeft: "5%",
+    },
+    absoluteWrapper: {
+        position: "absolute",
+        width: "100%",
+        height: "100%",
+        justifyContent: "center",
+        alignItems: "flex-end",
+    },
+});
+
+// @description 0: must read , 1: normal
+type NotiPriority = 0 | 1 | 2 | 3;
+
+type NotiLableProps = {
+    priority: NotiPriority;
+};
+function NotiLable(props: NotiLableProps) {
+    switch (props.priority) {
+        case 0:
+            return (
+                <View style={[NotiLableStyle.container, { backgroundColor: "#EC2222" }]}>
+                    <Text>필독</Text>
+                </View>
+            );
+        case 1:
+            return (
+                <View style={[NotiLableStyle.container, { backgroundColor: "#7C7C7C" }]}>
+                    <Text>필독</Text>
+                </View>
+            );
+        case 2:
+        case 3:
+
+        default:
+            return <></>;
+    }
+}
+
+const NotiLableStyle = StyleSheet.create({
+    container: {
+        width: "15%",
+        height: "50%",
+        borderRadius: 10,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+});
