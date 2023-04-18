918ecba41c4dac62bcd3f135888120eae2f9193b
최태성 <choetaeseong@choetaeseong-ui-MacBookAir.local>
2023-04-18 22:02:23 +0900

    font Theme 안에 넣음

--- src/hooks/themes/types.d.ts
+++ src/hooks/themes/types.d.ts
@@ -1,5 +1,5 @@
 declare namespace ThemeTypes {
-    type test = "normal" | "italic" | undefined
+    type test = "normal" | "italic" | undefined;
     interface ITheme {
         colors: {
             pallet: any;
@@ -13,17 +13,43 @@ declare namespace ThemeTypes {
                 lightgrey: string;
                 red: string;
                 white: string;
-            }
+            };
         };
-        css: {
-            font: {
-                universial: {
-                    fontFamily: string;
-                }
-        }
+        fonts: {
+            title1: {
+                fortFamily: string;
+                fontSize: Number;
+            };
+            title2: {
+                fortFamily: string;
+                fontSize: Number;
+            };
+            body1: {
+                fortFamily: string;
+                fontSize: Number;
+            };
+            body2: {
+                fortFamily: string;
+                fontSize: Number;
+            };
+            subTitle1: {
+                fortFamily: string;
+                fontSize: Number;
+            };
+            caption1: {
+                fortFamily: string;
+                fontSize: Number;
+            };
+            caption2: {
+                fortFamily: string;
+                fontSize: Number;
+            };
+            caption3: {
+                fortFamily: string;
+                fontSize: Number;
+            };
         };
-    };
-
+    }
     interface IThemes {
         DarkTheme: ITheme;
         LightTheme: ITheme;
