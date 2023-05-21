42711386a2e32fa815ce36c280d3e5c15866c862
seongbin-hong <qls056@gamil.com>
2023-05-21 23:25:03 +0900

    차량 수정 모달 뷰작업 완료

--- src/features/main/parking/screens/register_vehicle/types.tsx
+++ src/features/main/parking/screens/register_vehicle/types.tsx
@@ -1,6 +1,12 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { VillifeStackParamList } from "../../../../common/router/types";
+import { EtdaTime } from "../../blocks/etad_time_picker/types";
 
 type RegisterVehicleScreenProps = NativeStackScreenProps<VillifeStackParamList, "register_vehicle">;
+
+export type Vehicle = EtdaTime & {
+    plateNumber: string;
+    model: string;
+};
 
 export default RegisterVehicleScreenProps;
