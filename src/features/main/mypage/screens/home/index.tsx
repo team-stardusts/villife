b5cf406fba57e70b4ee6f7879ec48c0308369def
최태성 <choetaeseong@choetaeseong-ui-MacBookAir.local>
2023-06-22 00:37:43 +0900

    MyPage 더미 page 제작

--- src/features/main/mypage/screens/home/index.tsx
+++ src/features/main/mypage/screens/home/index.tsx
@@ -1,9 +1,11 @@
 import NoticeHomeScreenProps from "./type";
-import { SafeAreaView } from "react-native-safe-area-context";
 import React from "react";
 import NavigationView from "../../../../common/blocks/navigation";
-import useMyPageHomeScreenStyles from "./style";
 import useScreenMessage from "../../../../common/hooks/multilingual/hooks";
+import useStyler from "../../../../common/hooks/styler/hooks";
+import { View, Text } from "react-native";
+import { IconPerson } from "../../../../common/atoms/icon/human";
+import useMyPageHomeScreenStyles from "./style";
 
 export default function MyPageHomeScreen(props: NoticeHomeScreenProps) {
     const styles = useMyPageHomeScreenStyles();
@@ -13,12 +15,29 @@ export default function MyPageHomeScreen(props: NoticeHomeScreenProps) {
         <NavigationView
             headerOptions={{
                 title: message.messages.main.mypage.screen_title,
-                shown: true,
-                navComponentProps: {
-                    name: "Hello!",
-                },
-            }}>
-            <SafeAreaView style={styles.contentsWrapper}></SafeAreaView>
+            }}
+            bodyOptions={{ applyDefaultHorizontalPadding: false, applyDefaultVerticalPadding: false }}>
+            <View style={styles.topLevelBox}>
+                <View style={styles.infoContainer}>
+                    <View style={styles.leftContainer}>
+                        <IconPerson size={styles.humanIconSize.width as number} />
+                        <View style={styles.buttonContainer}>
+                            <Text style={styles.buttonContainerText}>내 정보 관리</Text>
+                        </View>
+                    </View>
+                    <View style={styles.rightContainer}>
+                        <Text style={styles.rightContainerTitleText}>김준우</Text>
+                        <Text style={styles.rightContainerSubText}>그린파크 501호</Text>
+                        <Text style={styles.rightContainerSubText}>BMW 120I / 108라 8477</Text>
+                        <Text style={styles.rightContainerSubText}>2022-04-05 ~ 2024-04-05</Text>
+                    </View>
+                </View>
+                <View style={styles.serviceContainer}>
+                    <Text style={styles.serviceContainerText}>내 동네 설정</Text>
+                    <Text style={styles.serviceContainerText}>내 동네 설정</Text>
+                    <Text style={styles.serviceContainerText}>내 동네 설정</Text>
+                </View>
+            </View>
         </NavigationView>
     );
 }
--- src/features/main/mypage/screens/home/style.ts
+++ src/features/main/mypage/screens/home/style.ts
@@ -1,10 +1,56 @@
 import { StyleSheet } from "react-native";
 import { UseMyPageHomeScreenStylesType } from "./type";
+import useStyler from "../../../../common/hooks/styler/hooks";
 
 export default function useMyPageHomeScreenStyles(): UseMyPageHomeScreenStylesType {
+    const { deviceUI, theme } = useStyler();
+
     return StyleSheet.create({
-        contentsWrapper: {
+        topLevelBox: {
             flex: 1,
+            flexDirection: "column",
+            backgroundColor: theme.colorFamily.white,
+        },
+        infoContainer: {
+            width: "100%",
+            height: deviceUI.screenSize.height * 0.2,
+            backgroundColor: theme.colorFamily.blue,
+            alignItems: "center",
+            justifyContent: "space-between",
+            flexDirection: "row",
+        },
+        leftContainer: { alignItems: "center", paddingLeft: deviceUI.moderateScale(30) },
+        humanIconSize: { width: deviceUI.moderateScale(85) },
+        buttonContainer: {
+            marginTop: deviceUI.moderateScale(10),
+            backgroundColor: theme.colorFamily.lightblue,
+            paddingVertical: deviceUI.moderateScale(5),
+            paddingHorizontal: deviceUI.moderateScale(10),
+            borderRadius: deviceUI.moderateScale(8),
+        },
+        buttonContainerText: { color: theme.colorFamily.white },
+        rightContainer: {
+            alignItems: "flex-end",
+            height: deviceUI.screenSize.height * 0.15,
+            justifyContent: "space-around",
+            paddingRight: deviceUI.moderateScale(30),
+        },
+        rightContainerTitleText: {
+            color: theme.colorFamily.white,
+            fontFamily: theme.font.researved.h3.fontFamily,
+            fontSize: theme.font.researved.h3.fontSize,
+        },
+        rightContainerSubText: {
+            color: theme.colorFamily.white,
+            fontFamily: theme.font.researved.h4.fontFamily,
+            fontSize: theme.font.researved.h5.fontSize,
+        },
+        serviceContainer: { marginLeft: deviceUI.screenSize.width * 0.05 },
+        serviceContainerText: {
+            color: theme.colorFamily.black,
+            fontFamily: theme.font.researved.h3.fontFamily,
+            fontSize: theme.font.researved.h3.fontSize,
+            marginTop: deviceUI.moderateScale(30),
         },
     });
 }
