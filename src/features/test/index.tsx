096fb9e37750981dc3594c2036b0bd45e3f3fb5a
malonabi <dnsi37>
2023-09-13 23:55:19 +0900

    테스트 페이지 변경

--- src/features/test/index.tsx
+++ src/features/test/index.tsx
@@ -1,14 +1,17 @@
 import { Linking, StyleSheet, Text, TouchableOpacity } from "react-native";
 import NavigationView from "../common/blocks/navigation";
 import useTestService from "./test_hook";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { View } from "react-native";
 import DotEnv from "../../libs/dotenv";
 import PaymentServiceProvider from "../main/payment/services/provider";
+import WebView from "react-native-webview";
 
 export default function TestScreen() {
     const test = useTestService();
     const env = new DotEnv();
+
+    const [paymentUrl, setPaymentUrl] = useState("");
 
     const styles = StyleSheet.create({
         container: {
@@ -22,36 +25,49 @@ export default function TestScreen() {
             alignItems: "center",
         },
     });
-    const openWebView = async () => {
+    const createOrder = async () => {
         const usecase = new PaymentServiceProvider();
         const result = await usecase.createOrder({
-            product_id: 977,
+            product_id: 945,
             product_type: "pt_management_fee",
-            product_name: "빌라이프 2월 관리비",
-            price: 40000,
+            product_name: "빌라이프 3월 관리비",
+            price: 80000,
         });
         if (result == null) return console.log("cannot open web view, failed to create order");
         console.log(result);
 
         const url = `${env.api.villife.REST_API_BASE_URL}payment/widget?order_unique_id=${result.unique_id}`; // 여기에 열고자 하는 URL을 입력하세요
+
+        setPaymentUrl(url);
         console.log(url);
-        Linking.openURL(url);
+        //Linking.openURL(url);
     };
 
-    useEffect(() => {}, [test?.name]);
+    useEffect(() => {
+        createOrder();
+    }, [test?.name]);
 
     return (
-        <NavigationView headerOptions={{ title: "TEST" }}>
+        <NavigationView
+            headerOptions={{ title: "TEST" }}
+            bottomNavOptions={{ shown: false }}
+            bodyOptions={{ applyDefaultHorizontalPadding: false }}>
             <View style={styles.container}>
-                <TouchableOpacity onPress={openWebView}>
-                    <Text>결제 테스트 하기</Text>
+                <TouchableOpacity onPress={createOrder}>
+                    <Text></Text>
                 </TouchableOpacity>
 
-                {/*   <WebView
-                    style={styles.container}
-                    source={{
-                        uri: `${env.api.villife.REST_API_BASE_URL}payment/widget?order_unique_id=eebf17a2_5151_4019_a2fa_b4d7426783292023_09_03_00_46_22`,
-                    }}></WebView> */}
+                {paymentUrl === "" ? (
+                    <></>
+                ) : (
+                    <WebView
+                        style={styles.container}
+                        javaScriptEnabled={true}
+                        injectedJavaScriptForMainFrameOnly={true}
+                        source={{
+                            uri: `${paymentUrl}`,
+                        }}></WebView>
+                )}
             </View>
         </NavigationView>
     );
