a4721a322026b26dd3a0cb85647ff9da2fba9fbe
seongbin-hong <qls056@gamil.com>
2024-01-10 23:58:15 +0900

    Admin selected building stroage 추가

--- src/features/common/hooks/service/user_info/service/index.ts
+++ src/features/common/hooks/service/user_info/service/index.ts
@@ -1,12 +1,18 @@
-import { useRecoilState } from "recoil";
+import { useRecoilState, useRecoilValue } from "recoil";
 import { adminInfoState } from "../../../states/atoms/user/admin_only";
 import VillifeNativeClient from "../../../../../../libs/villife-native-client";
 import Villife from "../../../../../../libs/villife-client/types";
+import { Storage } from "../../../../model/storage/type";
+import ViewModelStorage from "../../../../model/storage";
+import { loginDataState } from "../../../states/atoms/login";
 
 export default function useAdminInfoService() {
     const [adminInfo, setAdminInfo] = useRecoilState(adminInfoState);
+    const loginData = useRecoilValue(loginDataState);
 
     class AdminInfoService {
+        private static storage: Storage<number> = ViewModelStorage.getInstance() as Storage<number>;
+        private static storageKey: string = loginData?.name + "id-selected-building-id";
         private static readonly api = new VillifeNativeClient().user;
 
         public static async initializeAdminInformation(): Promise<void> {
@@ -15,11 +21,21 @@ export default function useAdminInfoService() {
                 return null;
             });
 
-            if (_admininfo !== null) {
+            const selectedBuildingId = await this.storage.getItem(this.storageKey);
+            let selectedBuilding = null;
+
+            if (_admininfo !== null && _admininfo.length > 0) {
+                if (selectedBuildingId !== null) {
+                    selectedBuilding = _admininfo.find((d) => d.id === selectedBuildingId);
+                }
+
+                if (selectedBuilding === undefined) {
+                    await this.storage.setItem(this.storageKey, _admininfo[0].id);
+                }
+
                 // 초기화 시 Managed buildings 중 첫 번째 Building으로 선택
                 setAdminInfo({
-                    ...adminInfo,
-                    selectedBuilding: _admininfo[0],
+                    selectedBuilding: selectedBuilding ?? _admininfo[0],
                     managedBuildings: _admininfo,
                 });
             }
@@ -28,6 +44,7 @@ export default function useAdminInfoService() {
         public static changeSelectedBulding(building: Villife.User.SimpleBuildingInfo) {
             if (adminInfo !== null) {
                 setAdminInfo({ ...adminInfo, selectedBuilding: building });
+                this.storage.setItem(this.storageKey, building.id);
             }
         }
 
